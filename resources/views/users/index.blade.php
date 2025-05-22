44d7988ab470e5ec39fa4cbf00a8e775fc767900
Ayatooo <louisreynard919@gmail.com>
2025-05-22 18:34:38 +0200

    :lipstick: feature/design - alert added to users/index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,6 +5,13 @@
             <flux:breadcrumbs.item>Annuaire des utilisateurs</flux:breadcrumbs.item>
         </flux:breadcrumbs>
 
+        <div class="bg-blue-50 border-t border-b border-blue-300 text-blue-600 px-4 py-3 rounded rounded-lg dark:bg-blue-900 dark:text-blue-100 dark:border-blue-700">
+            <p class="font-bold">Utilisateurs 🙋</p>
+            <p class="text-sm">
+                Retrouvez la liste de tous les utilisateurs de votre organisation. Vous pouvez en ajouter, les modifier ou les supprimer !
+            </p>
+        </div>
+
         <div class="w-fit">
             <flux:modal.trigger name="create-user">
                 <flux:button  variant="primary">
